7bb8ba5880cfa51e95dacf8d98e86ce18965abca
Mr-Kobra <red.kras.2013@gmail.com>
2022-05-13 17:33:21 +0300

    Create advantages template

---
+++ wordpress/template-parts/home/advantages-template.php
@@ -0,0 +1,27 @@
+<?php
+$home_id = get_field("home_page", "options");
+?>
+
+<section class="advantages" <?php if($id = get_field("advantages_id", $home_id)) { echo "id='$id'"; } ?>>
+    <div class="container">
+        <?php get_template_part("template-parts/heading-template", "", ["heading" => get_field("advantages_heading", $home_id)]) ?>
+        <?php if(get_field("advantages", $home_id)): ?>
+            <div class="advantages-container">
+                <?php while(has_sub_field("advantages", $home_id)): ?>
+                    <div class="advantages-card">
+                        <?php if($icon = get_sub_field("advantage_icon")): ?>
+                            <div class="advantages-icon">
+                                <img src="<?= $icon['url'] ?>" alt="<?= $icon["alt"] ?>">
+                            </div>
+                        <?php endif; ?>
+                        <?php if($content = get_sub_field("advantage_content")): ?>
+                            <div class="advantages-info">
+                                <?= $content ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                <?php endwhile; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+</section>
