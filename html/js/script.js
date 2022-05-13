2b26efde207a3074e826f0692601af87780481b1
Mr-Kobra <red.kras.2013@gmail.com>
2022-05-13 15:52:39 +0300

    Add scripts and popup windows

--- html/js/script.js
+++ html/js/script.js
@@ -1,5 +1,98 @@
-$(document).ready(function(){
+$(window).on("load", function() {
+    window.headerOffsetTop = $(".main-header").offset().top;
+    window.headerOuterHeight = $(".main-header").outerHeight();
+    window.scrollTop = $(window).scrollTop();
+
     $("#main-header_btn").on("click", function(){
-        $(this).toggleClass("active");
+        toggleMenuButton();
+        toggleMenu();
+    })
+
+    $(window).scroll(function (){
+        fixHeader();
+    })
+
+    fixHeader();
+
+    $(window).resize(function(){
+        setMenuCenter();
+        window.headerOuterHeight = $(".main-header").outerHeight();
+    })
+
+    $('.main-nav a').on("click", function(e){
+        e.preventDefault();
+        let block = $($(this).attr("href"));
+        toggleMenuButton();
+        toggleMenu();
+        setTimeout(function(){
+            let scroll = window.headerOffsetTop;
+            if(Math.round(block.offset().top) > Math.round(window.headerOffsetTop + window.headerOuterHeight)) {
+                fixHeader(true);
+                scroll = Math.round(block.offset().top - $(".main-header").outerHeight()) + 1;
+            }
+            $("html, body").animate({
+                scrollTop: scroll
+            }, 500);
+        }, 100)
+    })
+
+    $(".open-popup-btn").each(function(){
+        $(this).magnificPopup({
+            type: "inline"
+        })
     })
 })
+
+function fixHeader(force) {
+    let header = $(".main-header");
+    if(force || $(window).scrollTop() > window.headerOffsetTop) {
+        header.addClass("fixed");
+        $("body").css("padding-top", window.headerOuterHeight);
+    } else {
+        header.removeClass("fixed");
+        $("body").css("padding-top", 0);
+    }
+}
+
+function toggleMenu() {
+    let menu = $(".main-nav");
+    if(menu.hasClass("show")) {
+        if(window.scrollTop > window.headerOffsetTop) {
+            fixHeader(true);
+        }
+        enableScrollBar();
+        setTimeout(function(){
+            menu.removeClass("show");
+        }, 100);
+    } else {
+        menu.addClass("show")
+        setMenuCenter();
+        disableScrollBar();
+    }
+}
+
+function toggleMenuButton() {
+    $("#main-header_btn").toggleClass("active");
+}
+
+function disableScrollBar() {
+    window.scrollTop = $(window).scrollTop();
+    console.log(window.scrollTop);
+    $("body").css("overflow-y", "hidden");
+}
+
+function enableScrollBar() {
+    $("body").css("overflow-y", "");
+    setTimeout(function(){
+        $(window).scrollTop(window.scrollTop);
+    }, 100);
+    console.log(window.scrollTop);
+}
+
+function setMenuCenter() {
+    let menu = $(".main-nav");
+    if(menu.find("ul").outerHeight() < menu.height())
+        menu.addClass("center");
+    else
+        menu.removeClass("center");
+}
