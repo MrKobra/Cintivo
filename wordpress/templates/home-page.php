e9b33e7581ddb48610131ac854a1ba7c7b53c436
Mr-Kobra <red.kras.2013@gmail.com>
2022-05-13 17:33:33 +0300

    Add advantages template to home

--- wordpress/templates/home-page.php
+++ wordpress/templates/home-page.php
@@ -8,4 +8,7 @@ get_header();
     if(get_field("show_about") === "yes")
         get_template_part("template-parts/home/about-template");
 
+    if(get_field("show_advantages") === "yes")
+        get_template_part("template-parts/home/advantages-template");
+
 get_footer();
