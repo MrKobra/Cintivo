8971fe6d5f879dbfa5e6815d614bdc4962e111d4
Mr-Kobra <red.kras.2013@gmail.com>
2022-05-13 17:09:08 +0300

    Add footer

--- wordpress/footer.php
+++ wordpress/footer.php
@@ -11,22 +11,60 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'cintivo' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'cintivo' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'cintivo' ), 'cintivo', '<a href="http://underscores.me/">Mr-Kobra</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+    <footer class="main-footer">
+        <div class="container">
+            <div class="main-footer_container">
+                <div class="main-footer_info">
+                    <?php if($logo = get_field("footer_logo", "options")): ?>
+                        <div class="logo">
+                            <a href="<?= home_url("/") ?>">
+                                <img src="<?= $logo["url"] ?>" alt="<?= $logo["alt"] ?>">
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                    <p>Cintivolab© <?= date("Y") ?></p>
+                    <p>
+                        <?php if($link = get_field("public_offer", "options")) {
+                            echo "<a href='$link'>";
+                        } ?>
+                        Публичная оферта
+                        <?php if($link) {
+                            echo "</a>";
+                        } ?>
+                    </p>
+                </div>
+                <div class="main-footer_mailing">
+                    <div class="main-footer_mailing-container">
+                        <span>Подпишись на рассылку</span>
+                        <?= do_shortcode('[mailpoet_form id="1"]') ?>
+                    </div>
+                </div>
+                <div class="main-footer_contact">
+                    <div class="main-footer_contact-data">
+                        <h3>Связаться:</h3>
+                        <?php if($tel = get_field("telephone", "options")): ?>
+                            <div class="main-footer_contact-phone">
+                                <?= $tel ?>
+                            </div>
+                        <?php endif;
+                        if($mail = get_field("e_mail", "options")): ?>
+                            <div class="main-footer_contact-mail">
+                                <?= $mail ?>
+                            </div>
+                        <?php endif; ?>
+                    </div>
+                    <?php if($address = get_field("address", "options")): ?>
+                        <div class="main-footer_contact-data">
+                            <h3>Адрес:</h3>
+                            <div class="main-footer_contact-map">
+                                <?= $address ?>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 <?php wp_footer(); ?>
 
