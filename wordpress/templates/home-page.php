669d0fc88d9fdf554405177317621f0961b647e5
Mr-Kobra <red.kras.2013@gmail.com>
2022-05-14 09:54:19 +0300

    Add get consult template to home page

--- wordpress/templates/home-page.php
+++ wordpress/templates/home-page.php
@@ -14,4 +14,7 @@ get_header();
     if(get_field("show_contact") === "yes")
         get_template_part("template-parts/home/contact-template");
 
+    if(get_field("show_consult") === "yes")
+        get_template_part("template-parts/home/get-consult-template");
+
 get_footer();
