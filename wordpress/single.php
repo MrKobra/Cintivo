5193b13ce096cb23d4b47b2bc9554d75909e836a
Mr-Kobra <red.kras.2013@gmail.com>
2022-05-15 21:32:59 +0300

    Add default pages template

--- wordpress/single.php
+++ wordpress/single.php
@@ -10,31 +10,15 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'cintivo' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'cintivo' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
+    <section class="default-content">
+        <div class="container">
+            <h1><?php the_title(); ?></h1>
+            <?php if(have_posts()) {
+                the_post();
+                the_content();
+            } ?>
+        </div>
+    </section>
 
 <?php
-get_sidebar();
 get_footer();
